7c8aa589a6084aef051d1ee76196eea77055783f
Mirza Beg <themirza001@gmail.com>
2024-12-27 00:51:39 +0530

    adding the Java Task1 and Task3

--- java/src/main/java/com/prekdu/CollectionsExample.java
+++ java/src/main/java/com/prekdu/CollectionsExample.java
@@ -5,8 +5,47 @@ package com.prekdu;
  * HashSet. Also, create a HashMap by populating the words as key and frequency as the value in the
  * map. Iterate the list, set and map and print the content of the collection.
  */
-
+import java.util.*;
 public final class CollectionsExample {
-
-  public static void main(String[] args) {}
+    public static void performTask(String ...allStrings){
+        
+        //adding the Strings to ArrayList
+        ArrayList<String> myArrList=new ArrayList<>();
+        for(String currString:allStrings){
+            myArrList.add(currString);
+        }
+        
+        HashSet<String> myHashSet=new HashSet<>();
+        // // Adding the Strings to HashSet
+        for(String currString:allStrings){
+            myHashSet.add(currString);
+        }
+        
+        
+        HashMap<String,Integer>freqMap=new HashMap<>();
+        
+        // Adding the Strings to HashMap
+        for(String currString:allStrings){
+            freqMap.put(currString, freqMap.getOrDefault(currString, 0) + 1);
+        }
+        
+        //Traversing the ArrayList
+        for(String currString:myArrList){
+            System.out.println(currString);
+        }
+        
+        //Traversing the HashSet
+        for(String currString:myHashSet){
+            System.out.println(currString);
+        }
+        
+        //Traversing the HashMap
+        for (HashMap.Entry<String, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+      
+    }
+  public static void main(String[] args) {
+      performTask("Mirza", "is", "a", "fresher", "passionate", "about", "technology", "and", "impactful", "projects");
+  }
 }
--- java/src/main/java/com/prekdu/StringComparison.java
+++ java/src/main/java/com/prekdu/StringComparison.java
@@ -9,5 +9,29 @@ package com.prekdu;
  */
 
 public class StringComparison {
-  public static void main(String[] args) {}
+  public static void helper(String str1,String str2){
+      int len1=str1.length();
+      int len2=str2.length();
+      System.out.println("First String is:"+len1);
+      System.out.println("Second String is:"+len2);
+      
+      if(len1==len2){
+          System.out.println("Both String have Same length");
+      }else{
+           System.out.println("Both String have different length");
+      }
+      
+      if(str1==str2){
+          System.out.println("Both String are Same");
+      }else{
+          System.out.println("Both String are different");
+      }
+      
+  }
+  public static void main(String[] args)
+   {
+      String str1="mirzaBeg";
+      String str2="Atif_Beg";
+      helper(str1,str2);
+  }
 }
